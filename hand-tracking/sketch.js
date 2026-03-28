254bbcfd30c0aff3b40d7a78fdb248934f7d1c98
Kiet Tran <kietatr@gmail.com>
2026-03-28 15:03:22 -0500

    Repeating hands

--- hand-tracking/sketch.js
+++ hand-tracking/sketch.js
@@ -8,10 +8,12 @@
 
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 600;
+const EACH_HAND_REPS = 4;
+const EACH_HAND_SCALE = 1 / EACH_HAND_REPS;
 
 let video;
 let handPose;
-let hands = [];
+let handDetectionResults = [];
 let connections = [];
 
 async function setup() {
@@ -25,36 +27,61 @@ async function setup() {
     // start detecting hands from the webcam video
     handPose = await ml5.handPose();
     handPose.detectStart(video, (result) => {
-        hands = result;
+        handDetectionResults = result;
         connections = handPose.getConnections();
     });
 }
 
 function draw() {
     // Draw the webcam video
-    tint(100, 120);
+    tint(100, 100);
     image(video, 0, 0, width, height);
 
-    for (let i = 0; i < hands.length; i++) {
-        let hand = hands[i];
-        
-        // Draw connection lines
-        for (let i = 0; i < connections.length; i++) {
-            let connection = connections[i];
-            let startKeypoint = hand.keypoints[connection[0]];
-            let endKeypoint = hand.keypoints[connection[1]];
-            stroke(255, 0, 0, 100);
-            strokeWeight(4);
-            noFill();
-            line(startKeypoint.x, startKeypoint.y, endKeypoint.x, endKeypoint.y);
-        }
+    for (let i = 0; i < handDetectionResults.length; i++) {
+        let hand = handDetectionResults[i];
 
-        // Draw all the tracked hand points
-        for (let idxKeypoint = 0; idxKeypoint < hand.keypoints.length; idxKeypoint++) {
-            let keypoint = hand.keypoints[idxKeypoint];
-            fill(0, 255, 0, 100);
-            noStroke();
-            circle(keypoint.x, keypoint.y, 20);
+        if (hand.handedness == "Right") {
+            for (let j = 0; j < EACH_HAND_REPS; j++)
+            {
+                push();
+                translate(j*width*EACH_HAND_SCALE, 0);
+                scale(0.25);
+                drawHand(hand);
+                pop();
+            }
         }
+        else {
+            for (let j = 0; j < EACH_HAND_REPS; j++)
+            {
+                push();
+                translate(j*width*EACH_HAND_SCALE, height*EACH_HAND_SCALE);
+                scale(0.25);
+                drawHand(hand);
+                pop();
+            }
+            
+        }
+    }
+}
+
+function drawHand(hand)
+{
+    // Draw connection lines
+    for (let i = 0; i < connections.length; i++) {
+        let connection = connections[i];
+        let startKeypoint = hand.keypoints[connection[0]];
+        let endKeypoint = hand.keypoints[connection[1]];
+        stroke(255, 0, 0, 100);
+        strokeWeight(4);
+        noFill();
+        line(startKeypoint.x, startKeypoint.y, endKeypoint.x, endKeypoint.y);
+    }
+
+    // Draw all the tracked hand points
+    for (let idxKeypoint = 0; idxKeypoint < hand.keypoints.length; idxKeypoint++) {
+        let keypoint = hand.keypoints[idxKeypoint];
+        fill(0, 255, 0, 100);
+        noStroke();
+        circle(keypoint.x, keypoint.y, 20);
     }
 }
